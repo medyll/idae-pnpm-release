b4a5fd3d6f885ab103100080f22750f9a540af0d
medyll <43032494+medyll@users.noreply.github.com>
2026-01-31 11:34:09 +0100

    feat: add import statement for execa in publisher module

--- src/publisher.js
+++ src/publisher.js
@@ -1,3 +1,5 @@
+import { execa } from 'execa';
+
 export async function publishToRegistry(released, tag) {
     for (const pkg of released) {
       await execa('pnpm', ['publish', '--filter', pkg.name, '--tag', tag, '--no-git-checks'], { stdio: 'inherit' });
